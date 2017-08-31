738ba0219a50372c99c24f76ad489d0f78ac316e
lsj <754039701@qq.com>
2017-08-31 09:37:48 +0800

    Merge branch 'test1'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Thread/Test.java
+++ src/Thread/Test.java
@@ -28,6 +28,8 @@ public class Test {
             ready=true;
         }
     }
+
+
     // aaaaaaaaa
     // bbbbbb
 }
---
+++ src/Thread/Test2.java
@@ -0,0 +1,10 @@
+package Thread;
+
+/**
+ * Created by lsj on 17/8/31.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
