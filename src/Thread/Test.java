78cd08457e26440bd16af940b619232d5fd19491
lsj <754039701@qq.com>
2017-09-07 10:09:33 +0800

    Merge branch 'test1'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Thread/Test.java
+++ src/Thread/Test.java
@@ -1,32 +1,60 @@
 package Thread;
 
+import sun.awt.windows.ThemeReader;
+
 /**
  * Created by lsj on 17/8/30.
  */
 public class Test {
     private static int number;
     private static boolean ready;
-    private volatile int v;
+    private static volatile int v;
+    private int norm3;
+    private int t3;
 
     private static class ReaderThread extends Thread{
         @Override
         public void run() {
-            while (!ready){
-                System.out.println("1");
-                Thread.yield();
-                System.out.println("3");
+            int x = number;
+            try {
+                Thread.sleep(2000);
+                System.out.println("V2:  " + v);
+                v = 2;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            System.out.println(number);
+            x += 1;
+            number = x;
+        }
+    }
+
+    private static class ReaderThread2 extends Thread{
+        @Override
+        public void run() {
+            int x = number;
+            try {
+                Thread.sleep(1000);
+                System.out.println("V2:  " + v);
+                v = 2;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            x += 1;
+            number = x;
         }
     }
 
     public static void main(String[] args) {
-        while(true) {
-            ready = false;
-            new ReaderThread().start();
-            number = 42;
-            ready=true;
+        number = 0;
+        new ReaderThread().start();
+        new ReaderThread().start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println(number);
     }
 
 
---
+++ src/Thread/test3.java
@@ -0,0 +1,7 @@
+package Thread;
+
+/**
+ * Created by lsj on 17/9/7.
+ */
+public class test3 {
+}
